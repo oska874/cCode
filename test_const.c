dbd705f5d91a1b2d50c5c9aa57a0a201de50bafe
ezio <ezio_zhang@outlook.com>
2015-08-27 11:27:46 +0800

    init
    1. const test
    2. va_arg(char/int)

---
+++ test_const.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main()
+{
+	const int main1 = 10;
+	main1=11;
+	print("%x\n",main1);
+	return 0;
+}
---
+++ test_vararg.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+#include "stdarg.h"
+#include "string.h"    
+
+int vararg(char *msg,...);
+int vararg2(int cmd,...);
+
+int main()
+{
+	vararg("abc","def","ghia","/0");
+    vararg2(1,2);
+	return 0;
+}
+
+int vararg(char *msg,...)
+{
+	va_list argp;                   /* 定义保存函数参数的结构 */    
+    int argno = 0;                  /* 纪录参数个数 */    
+    char *para;                     /* 存放取出的字符串参数 */    
+    va_start( argp, msg );    /* argp指向传入的第一个可选参数，    msg是最后一个确定的参数 */
+    while (1)   
+    {    
+        para = va_arg( argp, char *);                 /*    取出当前的参数，类型为char *. */    
+        if ( strcmp( para, "/0") == 0 )               /* 采用空串指示参数输入结束 */    
+            break;    
+        printf("Parameter #%d is: %s\n", argno, para);    
+        argno++;    
+    }    
+    va_end( argp );           
+    return 0;
+}
+
+int vararg2(int cmd,...)
+{
+    va_list argp;
+    int argno = 0;
+    int para;
+    va_start(argp,cmd);
+    switch(cmd)
+    {
+        case 0: 
+            para = va_arg(argp,int);
+            printf("AA:%d  %d \n",cmd,para);
+            break;
+        case 1:
+            para = va_arg(argp,int);
+            printf("bb:%d  %d \n",cmd,para);
+            break;
+        default:
+            printf("1111\n");
+            break;
+
+    }
+    va_end(argp);
+    return argno;
+}
