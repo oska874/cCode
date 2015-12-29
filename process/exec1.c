c053a3e727c9e6ba4300bf3e6ffbca82603fe36e
ezio <ezio_zhang@outlook.com>
2015-12-29 17:40:32 +0800

    atexit

--- process/exec1.c
+++ process/exec1.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
+#include <stdlib.h>
 
+void hello(void)
+{
+    printf("bye\n");
+}
 int main(void)
 {
-    int ret;
-    ret = execl("/usr/bin/vim", "vi", NULL);
+    int ret = 0;
+    //ret = execl("/usr/bin/vim", "vi", NULL);
     if (ret ==-1){
         perror("open fail\n");
         exit(0);
     }
+    atexit(hello);
 
     return 0;
 }
