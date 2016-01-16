5bce75e6048388d734bdfc43363d739749edf7a6
ezio <ezio_zhang@outlook.com>
2016-01-16 15:43:53 +0800

    update build procedure:add arm and x86

--- cmake/build.sh
+++ cmake/build.sh
@@ -11,14 +11,24 @@ fi
 
 rm -rf ./build/*
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=../p1020_toolchain.cmake ..
-make
-powerpc-unknown-linux-gnuspe-objdump -D RTS > RTSdump
-
 if [ $# -eq 1 ]
 then
-    if [ $1 = "qemu" ]
+    if [ $1 = "ppc" ]
     then
-        qemu-ppc -cpu e500v2_v22 RTS
+        cmake -DCMAKE_TOOLCHAIN_FILE=../p1020_toolchain.cmake ..
+        make
+        powerpc-unknown-linux-gnuspe-objdump -D RTS > RTSdump
+    elif [ $1 = "arm" ]
+    then
+        cmake -DCMAKE_TOOLCHAIN_FILE=../a9_toolchain.cmake ..
+        make
+        arm-linux-gnueabihf-objdump -D RTS > RTSdump
+    elif [ $1 = "local" ]
+    then
+        cmake  ..
+        make
+        objdump -D RTS > RTSdump
     fi
 fi
+
+
