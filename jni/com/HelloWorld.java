418b9889eef7e8d37c484bb2f9c535ac54ce27d2
ezio <ezio_zhang@outlook.com>
2016-08-28 22:13:18 +0800

    add jni test

---
+++ jni/com/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com;
+
+/**
+ * @author magc
+ *
+ */
+public class HelloWorld {
+    static {
+        System.loadLibrary("Hello");
+    }
+
+    public native void DisplayHello();
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        new HelloWorld().DisplayHello();
+    }
+
+}
