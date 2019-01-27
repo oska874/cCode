1b2105a746450b44676f3ad236b715c8d81022f4
oska874 <ezio_zhang@outlook.com>
2019-01-27 17:20:45 +0800

    fix

--- other/zip_test_2.c
+++ other/zip_test_2.c
@@ -15,6 +15,10 @@ struct file_info {
     mode_t mode;
 };
 
+int32_t delete_file_dir(char *path)
+{
+    return remove(path);
+}
 int pmkdir(const char *path)
 {
     char name[strlen(path)];
@@ -55,7 +59,7 @@ int32_t compress_files(char **fnames, int32_t file_num, char *compressed, int32_
         z = zip_open(compressed, ZIP_CREATE|ZIP_EXCL, &err);
         if(z != NULL) {
             for(int32_t i=0; i<file_num; i++) {
-                printf("%s\n", fnames[i]);
+                //printf("%s\n", fnames[i]);
                 s = zip_source_file(z, fnames[i], 0, -1);
                 if(s!=NULL) {
                     if (zip_file_add(z, fnames[i], s, ZIP_FL_OVERWRITE|ZIP_FL_ENC_GUESS)<0) {
@@ -75,7 +79,7 @@ int32_t compress_files(char **fnames, int32_t file_num, char *compressed, int32_
                 ret = 0;
             }
         } else {
-            ret = -2;
+            ret = -3;
         }
     }
     return ret;
@@ -109,31 +113,36 @@ int32_t uncompress_file(char *compressed, char *dest_dir, int32_t type)
                 memcpy(dest_path_joint, dest_dir, plen);
                 dest_path_joint[plen] = '/';
 #endif
-
                 z = zip_open(compressed, ZIP_CREATE, &err);
                 if (z != NULL) {
                     zip_int64_t f_cnt = zip_get_num_entries(z, ZIP_FL_UNCHANGED);
                     for(zip_int64_t i=0; i<f_cnt; i++) {
                         const char *name = zip_get_name(z, i, ZIP_FL_ENC_GUESS);
-                        printf("find %s\n", name);
+                        //printf("find %s\n", name);
                         char *d = strdup(name);
                         if( d != NULL) {
                             char *p = strrchr(d, '/');
                             if(p != NULL) {
 #if 1
                                 p++;
-                                printf("A %s,%d\n",p,strlen(p));
-                                printf("!?%s,%d\n",dest_path_joint,strlen(dest_path_joint));
+                                //printf("A %s,%d\n",p,strlen(p));
+                                //printf("!?%s,%d\n",dest_path_joint,strlen(dest_path_joint));
                                 memcpy(&dest_path_joint[plen], p, strlen(p));
                                 dest_path_joint[plen+strlen(p)] ='\0';
 #endif
                                 //*p = '\\0';
                                 //pmkdir(d);
                             }
+                            else{
+#if 1
+                                //printf("B %s,%d\n",d,strlen(d));
+                                memcpy(&dest_path_joint[plen], d, strlen(d));
+                                dest_path_joint[plen+strlen(d)] ='\0';
+#endif
+                            }
                             free(d);
-
-                            printf("!%s %d\n",dest_path_joint,strlen(dest_path_joint));
-                            int32_t fd = open(dest_path_joint, O_WRONLY|O_CREAT);
+                        //    printf("!%s %d\n",dest_path_joint,strlen(dest_path_joint));
+                            int32_t fd = open(dest_path_joint, O_WRONLY|O_CREAT|O_TRUNC, 0777);
                             struct zip_file *f = zip_fopen(z, name, 0);
                             if (f != NULL && fd > 0) {
                                 zip_int64_t j, n = 0;
@@ -146,7 +155,7 @@ int32_t uncompress_file(char *compressed, char *dest_dir, int32_t type)
                                 zip_fclose(f);
                             }
                         } else {
-                            printf("low mem\n");
+                            perror("low mem\n");
                             ret = -2;
                         }
                     }
@@ -172,7 +181,7 @@ int main()
     int32_t ret=0;
     char **names;
     char *name_buf;
-    char tmp[]="/home/ezio/src/cCode/other/a\0";
+    char tmp[]="a\0";//"/home/ezio/src/cCode/other/a\0";
     name_buf = malloc(10*(strlen(tmp)+1));
     if(name_buf == 0) {
         perror("req nb fail");
@@ -184,23 +193,29 @@ int main()
         return -2;
     }
 
-    printf("sx0\n");
     size_t nlen = strlen(tmp);
-    printf("l %d\n",nlen);
+//    printf("%d\n",nlen);
     for(int i=0; i<8; i++) {
         tmp[nlen-1]+=1;
         memcpy(&name_buf[i*(nlen+1)], tmp, nlen+1);
         names[i] = &name_buf[i*(nlen+1)];
-        //    printf("%s\n",names[i]);
+  //      printf("%s , %d %d\n",names[i],strlen(tmp),strlen(names));
     }
-    printf("s\n");
+    printf("compress zip\n");
     ret = compress_files(names, 8, "alpha.zip", 0);
     if(ret <0) {
         printf("compress fail %d\n",ret);
     }
+    printf("uncompress zip\n");
     ret = uncompress_file("/home/ezio/src/cCode/other/alpha.zip","/home/ezio/src/cCode/other/x/",0);
     if(ret <0) {
         printf("uncompress fail %d\n",ret);
+    }
+    printf("delete zip\n");
+    ret = delete_file_dir("/home/ezio/src/cCode/other/alpha.zip");
+    if(ret != 0){
+        perror("delete fail");
+        printf("path %s\n", "/home/ezio/src/cCode/other/alpha.zip");
     }
     return 0;
 }
